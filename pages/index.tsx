0f44c268ed7f56c1af6dae46d0562e933d094c88
mgroovyank <mayanksamariya99@gmail.com>
2025-01-29 18:06:25 +0530

    ignore-duplicate-scans

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,37 @@
+import { Result } from "@zxing/library";
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
 
 export default function Page() {
   const [data, setData] = useState("No result");
+  const [lastScanned, setLastScanned] = useState("");
 
   const scannerConstraints: MediaTrackConstraints = {
     facingMode: "environment",
   };
 
+  function handleScanResult(result: Result) {
+    console.log(result);
+    setData(result.getText());
+    const uuid: string = result.getText();
+    console.log("uuid: " + uuid);
+    if (uuid == lastScanned) {
+      console.log("Already Scanned!!!");
+      return;
+    }
+  }
+
   return (
     <>
       <QrReader
-        scanDelay={300}
+        scanDelay={1000}
         onResult={(result, error) => {
           if (!!result) {
-            setData(result.getText());
+            handleScanResult(result);
           }
 
           if (!!error) {
-            console.info(error);
+            console.log(error);
           }
         }}
         constraints={scannerConstraints}
